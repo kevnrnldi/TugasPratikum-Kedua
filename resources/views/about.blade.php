dc2b4bd9e5331479be2fe412a5e41bd0d332c9a3
mkevinrinaldi <mkevinrinaldi@gmail.com>
2024-09-29 14:50:30 +0700

    masuk

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('home')
+@section('tentangku')
+<div class="container-fluid px-4">
+<h1 class="mt-4">About</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+            <li class="breadcrumb-item active">About</li>
+        </ol>
+
+        
+</div>
+@endsection
